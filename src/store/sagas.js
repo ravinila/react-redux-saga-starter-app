3b99400142cf0f7e2e42f7c7a2cb8892d754ab21
Ravichandran <ravi.nila@live.com>
2020-04-13 16:38:17 +0530

    Update sagas.js

--- src/store/sagas.js
+++ src/store/sagas.js
@@ -1,6 +1,10 @@
 import { all } from "redux-saga/effects";
 import { sagas as userSagas } from "./users";
+// import { sagas as otherSagas } from "./otherModule";
 
 export default function*() {
-  yield all([...userSagas.map(saga => saga())]);
+  yield all([
+    ...userSagas.map(saga => saga()),
+    //...otherSagas.map(saga => saga()),
+  ]);
 }
